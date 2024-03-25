dae49e92337142bfeef1f852c836a679ceb70d8a
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2024-03-25 20:47:09 +0100

    all files

---
+++ leaderboard/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { InternsBoard } from './components/InternsBoard'
+import './components/Styles.css'
+
+function App() {
+  return (
+    <div id='main'>
+      <InternsBoard/>
+    </div>
+  )
+}
+
+export default App
---
+++ leaderboard/src/components/InternsData.js
@@ -0,0 +1,72 @@
+export const InternsData = [
+    {
+        id: 1,
+        name: "Adaeze Ugwuoke",
+        LearningTrack: "Frontend",
+        score : 9550,
+        img: "./image/intern1.jpg",
+    },
+    {
+        id: 2,
+        name: "Chris Obi",
+        LearningTrack: "Frontend",
+        score : 2310,
+        img: "",
+    },
+    {
+        id: 3,
+        name: "Tomi Adeola",
+        LearningTrack: "Frontend",
+        score : 350,
+        img: ""
+    },
+    {
+        id: 4,
+        name: "Musa Ibrahim",
+        LearningTrack: "Frontend",
+        score : 2100,
+        img: ""
+    },
+    {
+        id: 5,
+        name: "Chiamaka Eze",
+        LearningTrack: "Frontend",
+        score : 1250,
+        img: ""
+    },
+    {
+        id: 6,
+        name: "Peace Odo",
+        LearningTrack: "Frontend",
+        score : 5250,
+        img: ""
+    },
+    {
+        id: 7,
+        name: "Bube Williams",
+        LearningTrack: "Frontend",
+        score : 3250,
+        img: ""
+    },
+    {
+        id: 8,
+        name: "Peace Odo",
+        LearningTrack: "Frontend",
+        score : 6750,
+        img: ""
+    },
+    {
+        id: 9,
+        name: "Levi Chukwu",
+        LearningTrack: "Frontend",
+        score : 7620,
+        img: ""
+    },
+    {
+        id: 10,
+        name: "Mirable Ugwu",
+        LearningTrack: "Frontend",
+        score : 8950,
+        img: ""
+    }
+]
